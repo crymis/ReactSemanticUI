8d66f321eafd510a9c7cadac43e4ca27cc82d0ba
Daniel Eckelt <deckelt@invoex.de>
2017-05-22 15:07:47 +0200

    Install material ui npm package.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import App from './App';
 import './index.css';
 
