7a3d47e514fac17b27cd7b3fd9d3995ff885004f
Daniel Eckelt <deckelt@invoex.de>
2017-05-22 13:03:11 +0200

    Add hornbach font as default. Start styling buttons. Fix animation (order of importing css rules fixed => semantic.css first)

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,9 @@
+import '../semantic/dist/semantic.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import '../semantic/dist/semantic.min.css';
 
 ReactDOM.render(
   <App />,
