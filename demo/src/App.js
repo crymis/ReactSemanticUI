4433dd0f70558b04a1b98ce19dc562ba8d98d040
Daniel Eckelt <deckelt@invoex.de>
2017-05-23 10:38:47 +0200

    Set up bootstrap.

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,10 +4,12 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 import logo from './logo.svg';
 import './App.css';
-import { Button, Icon, Grid, Rating, Popup, Dropdown, Message } from 'semantic-ui-react';
 import CardGallery from './components/CardGallery';
 import { getSomeImages } from './data/images';
 import { languageOptions } from './data/text';
+
+import Button from 'react-bootstrap/lib/Button';
+
 
 class App extends React.Component {
 
@@ -50,73 +52,85 @@ class App extends React.Component {
 
   render() {
     return (
-      <Grid padded="vertically">
-        <Grid.Row>
-          <Grid.Column>
-            <div className="App">
-              <div className="App-header">
-                <Dropdown 
-                id="language-selection"
-                className="icon" 
-                floating labeled button 
-                defaultValue={languageOptions[0].value}
-                options={languageOptions}
-                onChange={(e, props) => this.changeLanguage(e,props)} />
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-              </div>
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row centered>
-          <Grid.Column>
-            <p className="App-intro">Try out Semantic UI React</p>
-            <Popup content='How many images?' position='right center' trigger={
-              <Rating 
-                ref={(rating) => {this.numberOfImages = rating; }} 
-                maxRating={8} 
-                defaultRating={1} 
-                icon='star'
-                onRate={(e, props) => this.renderImageCards(e, props)}/>
-              } />
-            <br />
-            <Button 
-              animated='vertical'
-              onClick={(e) => this.toggleClick(e)}
-              ref={(btn) => {this.mainButton = btn; }}>
-              <Button.Content visible>{!this.state.clicked ? 'Show Cards' : 'Hide Cards'}</Button.Content>
-              <Button.Content hidden>
-                <Icon name={`${!this.state.clicked ? 'down' : 'up'} arrow`}></Icon>
-              </Button.Content>
-            </Button>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row centered>
-          <Grid.Column width={15}>
-            {this.state.clicked ? this.renderImageCards() : null}
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-              <CSSTransitionGroup
-              transitionName='slideUp'
-              transitionEnterTimeout={400}
-              transitionLeaveTimeout={400}>
-                {this.state.notSupported ? 
-                <Message icon id="message">
-                  <Icon name='translate' />
-                  <Message.Content>
-                    <Message.Header>Not Supported!</Message.Header>
-                    This language is not supported yet.
-                  </Message.Content>
-                </Message>
-                : null}
-            </CSSTransitionGroup>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Button bsStyle="info">Show Cards</Button>
+      </div>
+    )
   }
+
+  // render() {
+  //   return (
+  //     <Grid padded="vertically">
+  //       <Grid.Row>
+  //         <Grid.Column>
+  //           <div className="App">
+  //             <div className="App-header">
+  //               <Dropdown 
+  //               id="language-selection"
+  //               className="icon" 
+  //               floating labeled button 
+  //               defaultValue={languageOptions[0].value}
+  //               options={languageOptions}
+  //               onChange={(e, props) => this.changeLanguage(e,props)} />
+  //               <img src={logo} className="App-logo" alt="logo" />
+  //               <h2>Welcome to React</h2>
+  //             </div>
+  //           </div>
+  //         </Grid.Column>
+  //       </Grid.Row>
+  //       <Grid.Row centered>
+  //         <Grid.Column>
+  //           <p className="App-intro">Try out Semantic UI React</p>
+  //           <Popup content='How many images?' position='right center' trigger={
+  //             <Rating 
+  //               ref={(rating) => {this.numberOfImages = rating; }} 
+  //               maxRating={8} 
+  //               defaultRating={1} 
+  //               icon='star'
+  //               onRate={(e, props) => this.renderImageCards(e, props)}/>
+  //             } />
+  //           <br />
+  //           <Button 
+  //             animated='vertical'
+  //             onClick={(e) => this.toggleClick(e)}
+  //             ref={(btn) => {this.mainButton = btn; }}>
+  //             <Button.Content visible>{!this.state.clicked ? 'Show Cards' : 'Hide Cards'}</Button.Content>
+  //             <Button.Content hidden>
+  //               <Icon name={`${!this.state.clicked ? 'down' : 'up'} arrow`}></Icon>
+  //             </Button.Content>
+  //           </Button>
+  //         </Grid.Column>
+  //       </Grid.Row>
+  //       <Grid.Row centered>
+  //         <Grid.Column width={15}>
+  //           {this.state.clicked ? this.renderImageCards() : null}
+  //         </Grid.Column>
+  //       </Grid.Row>
+  //       <Grid.Row>
+  //         <Grid.Column>
+  //             <CSSTransitionGroup
+  //             transitionName='slideUp'
+  //             transitionEnterTimeout={400}
+  //             transitionLeaveTimeout={400}>
+  //               {this.state.notSupported ? 
+  //               <Message icon id="message">
+  //                 <Icon name='translate' />
+  //                 <Message.Content>
+  //                   <Message.Header>Not Supported!</Message.Header>
+  //                   This language is not supported yet.
+  //                 </Message.Content>
+  //               </Message>
+  //               : null}
+  //           </CSSTransitionGroup>
+  //         </Grid.Column>
+  //       </Grid.Row>
+  //     </Grid>
+  //   );
+  // }
 }
 
 export default App;
